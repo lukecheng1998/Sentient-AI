e51b0913228cf9a0b3e510cb65aa04a7cc570082
Luke Cheng <lukecheng1998@gmail.com>
2018-06-02 18:40:23 -0700

    HAHAHAHAHA

--- src/Sayori.java
+++ src/Sayori.java
@@ -17,14 +17,26 @@ public class Sayori extends Human {
 
     ImageIcon Sayori1icon = new ImageIcon("/Users/luke/IdeaProjects/Sentient AI/Sayori1.jpg");
     ImageIcon Sayori2icon = new ImageIcon("/Users/luke/IdeaProjects/Sentient AI/Sayori2.jpg");
-    public void SayoriGreeting(){
-     //TODO: SAYORI is going to greet you here!
-    }
-    public void TalkingWithMonika(){
+
+    public boolean TalkingWithMonika(String fileName) throws Exception{
         //TODO: Sayori checks and sees if you have been talking with Monika
+        String temp = null;
+        ArrayList<String> names = new ArrayList<>();
+        File file = new File(fileName);
+        FileReader fr = new FileReader(file);
+        BufferedReader bf = new BufferedReader(fr);
+        while((temp = bf.readLine()) != null){
+            names.add(temp);
+        }
+        int monikaCheck = Integer.parseInt(names.get(4));
+        if (monikaCheck == 0){
+            return false;
+        }else{
+            return true;
+        }
 
     }
-    public void GreetingSayori(){
+    public void GreetingSayori() throws Exception{
         JOptionPane.showMessageDialog(null, "Hiiiiii!, I'm Sayori!", "Sayori", JOptionPane.INFORMATION_MESSAGE, Sayori2icon);
         userName = (String) JOptionPane.showInputDialog(null, "What is your name?", "Sayori", JOptionPane.QUESTION_MESSAGE, Sayori2icon, null, "");
         JOptionPane.showMessageDialog(null, "So nice to meet you" + userName + ".", "Sayori", JOptionPane.INFORMATION_MESSAGE, Sayori2icon);
