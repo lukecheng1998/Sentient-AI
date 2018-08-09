8bb9e6fed2d4adee6e001af7a72e83bb9d521766
Luke Cheng <lukecheng1998@gmail.com>
2018-08-09 00:29:39 -0400

    More Natsuki Stuff

--- src/Natsuki.java
+++ src/Natsuki.java
@@ -1,2 +1,11 @@
-public class Natsuki {
+import java.awt.*;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Random;
+import javax.swing.*;
+public class Natsuki extends Human{
+    public Natsuki(String name, int age, String eyeColor){
+        super(name, age, eyeColor);
+    }
 }
